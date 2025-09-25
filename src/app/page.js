67325beb5d7668260883d191b81def6a54cf4962
rijans <makwanarijans@gmail.com>
2025-09-25 22:24:39 +0530

    feat(RecentlyAdded) : CSR

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,16 @@
+import FeaturedProduct from '@/components/home/FeaturedProduct'
 import HeroSection from '@/components/home/HeroSection'
+
+import RecentlyAdded from '@/components/home/RecentlyAdded'
 import React from 'react'
 
 function Home() {
   return (
    <>
+    <RecentlyAdded></RecentlyAdded>
    <HeroSection></HeroSection>
+   <FeaturedProduct></FeaturedProduct>
+  
    </>
   )
 }
